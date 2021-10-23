38c1f7375b186353cfce57e14d40ecf90d1fc50e
judyann03 <judyanndayo03@gmail.com>
2021-10-23 22:11:37 +0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'dc-angular';
+
+  currentRate = 0;
+}
